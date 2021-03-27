de653f5656dffbc58b523e64916a231b2a44d4f4
Ash Shah <ashni@uw.edu>
2021-03-27 13:30:02 -0700

    navbar color changes

---
+++ pages/issue/blog.js
@@ -0,0 +1,12 @@
+import NavBar from '../../components/NavBar.js'
+import styles from '../../styles/Blog.module.css'
+
+
+export default function Blog() {
+    return (
+        <div className={styles["full-page"]}>
+            <NavBar></NavBar>
+        </div>
+        
+    )
+}
